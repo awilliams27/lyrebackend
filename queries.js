598d5266e20befa091e5a66bbba469a4e20abbed
Alex Williams <alexwilliams567@gmail.com>
2018-03-01 13:04:23 -0500

    environemtn var for pg db

--- queries.js
+++ queries.js
@@ -6,7 +6,7 @@ var options = {
 };
 
 var pgp = require("pg-promise")(options);
-var connectionString = "postgres://testpuppies:testpuppies@localhost:5432/lyre";
+var connectionString = process.env.DATABASE_URL;
 var db = pgp(connectionString);
 
 // add query functions
